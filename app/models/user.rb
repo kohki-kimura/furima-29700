f78a91c898ae7bc8420e7683c894863de74eb27e
kohki-kimura <kohki.soccer0217@gmail.com>
2020-09-21 19:42:31 +0900

    バリデーションの削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
          validates :password,format: { with: /[a-z\d]{6,}/ }
-         validates :email, uniqueness: true, format: { with: /@.+/ }
+         validates :email, format: { with: /@.+/ }
          validates :first_name,:family_name,   format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
          validates :first_name_kana,:family_name_kana,   format: { with: /\A[ァ-ン]+\z/ }
           
