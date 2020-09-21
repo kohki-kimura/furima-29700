9fd0b048811822907d7e0d825a5a841b4b0df542
kohki-kimura <kohki.soccer0217@gmail.com>
2020-09-21 20:37:20 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-         validates :password,format: { with: /[a-z\d]{6,}/ }
+
+         validates :password,format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/ }
          validates :email, format: { with: /@.+/ }
          validates :first_name,:family_name,   format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
          validates :first_name_kana,:family_name_kana,   format: { with: /\A[ァ-ン]+\z/ }
