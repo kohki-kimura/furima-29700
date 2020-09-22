f110a29878aa417af3bd36a9efaee3326458cb40
kohki-kimura <kohki.soccer0217@gmail.com>
2020-09-22 15:16:04 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          validates :nickname, presence: true
          validates :birthday, presence: true
-         validates :password,format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
-         validates :first_name,:family_name,   format: { with: /\A[ぁ-んァ-ン一-龥]+\z/i}
-         validates :first_name_kana,:family_name_kana,   format: { with: /\A[ァ-ン]+\z/i}
+         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+         validates :first_name,:family_name, presence: true,  format: { with: /\A[ぁ-んァ-ン一-龥]+\z/i}
+         validates :first_name_kana,:family_name_kana,   presence: true, format: { with: /\A[ァ-ン]+\z/i}
           
 end
