d1ebe7adeeb0fdcdc008b954c668522f4e9bc3c6
kohki-kimura <kohki.soccer0217@gmail.com>
2020-09-22 22:49:42 +0900

    エラー文の表示
    _errors~erbのファイルはmodelのバリデーションを読み込んでエラー文を表示させる記述があるが、new.html.erbで参照するモデルが@itemではなく@userになってしまっていたことが原因でエラーが出た。なのでまずはコントローラーで@itemを作成し、それをform_withにも正しく記述した。

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category,:condition,:fee,:day,:address
+
+  validates :name, :description, :price, :user, :category,:condition,:fee,:day,:address, presence: true
+
+  validates :price, format: { with: /\A[300-9999999]+\z/}
+
+  validates :category_id, :condition_id, :fee_id, :day_id, :address_id, numericality: { other_than: 1 } 
 end
