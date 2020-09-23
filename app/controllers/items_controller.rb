091c0dc0bca722d695bbc3665730892065ddba55
kohki-kimura <kohki.soccer0217@gmail.com>
2020-09-23 11:14:52 +0900

    ストロングパラメーターの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,15 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to root_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :user, :category_id, :condition_id, :fee_id, :day_id, :address_id).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,14 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category,:condition,:fee,:day,:address
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :fee
+  belongs_to_active_hash :day
+  belongs_to_active_hash :address
 
-  validates :name, :description, :price, :user, :category,:condition,:fee,:day,:address, presence: true
+  belongs_to :user
 
-  validates :price, format: { with: /\A[300-9999999]+\z/}
-
+  validates :name, :description, :price, :user, :category, :condition, :fee, :day, :address, presence: true
+  # validates :price, format: { with: /\A[300-9999999]+\z/}
   validates :category_id, :condition_id, :fee_id, :day_id, :address_id, numericality: { other_than: 1 } 
 end
