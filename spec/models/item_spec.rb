90ec841a19354a009357621f717f290dd910208c
kohki-kimura <kohki.soccer0217@gmail.com>
2020-09-23 21:12:43 +0900

    テストコードの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -39,47 +39,60 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include("Price is not a number")
     end
-
     it "category_idが空だと登録できない" do
       @item.category_id = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Category is not a number")
     end
-
+    it "category_idが1だと登録できない" do
+      @item.category_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category must be other than 1")
+    end
     it "condition_idが空だと登録できない" do
       @item.condition_id = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Condition is not a number")
     end
-
+    it "condition_idが1だと登録できない" do
+      @item.condition_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Condition must be other than 1")
+    end
     it "fee_idが空だと登録できない" do
       @item.fee_id = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Fee is not a number")
     end
-
+    it "fee_idが1だと登録できない" do
+      @item.fee_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Fee must be other than 1")
+    end
+    it "day_idが空だと登録できない" do
+      @item.day_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Day is not a number")
+    end
+    it "day_idが1だと登録できない" do
+      @item.day_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Day must be other than 1")
+    end
+    it "addressが空だと登録できない" do
+      @item.address_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Address is not a number")
+    end
+    it "address_idが1だと登録できない" do
+      @item.address_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Address must be other than 1")
+    end
     it "imageが空だと登録できない" do
       @item.image = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Image can't be blank")
     end
-
-  end
-end
-
-RSpec.describe "出品ページへのアクセス", type: :system do
-  it "ログインしていないユーザーは、出品ページにアクセスできず、ログイン画面に遷移する" do
-    visit new_item_path
-    expect(current_path).to eq new_user_session_path
-  end
-  it "ログインしているユーザーは出品ページにアクセスできる"do
-    @user = FactoryBot.create(:user)
-    visit  new_user_session_path
-    fill_in 'email', with: @user.email
-    fill_in 'password', with: @user.password
-    click_on("ログイン")
-    expect(current_path).to eq root_path
-    visit new_item_path
-    expect(current_path).to eq new_item_path
   end
 end
