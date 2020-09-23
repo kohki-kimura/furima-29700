c576cfb6bc3c492d78d0030945bc1072efbf435d
kohki-kimura <kohki.soccer0217@gmail.com>
2020-09-23 12:04:59 +0900

    エラー文の表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,11 +5,16 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
   def create
     @item = Item.new(item_params)
-    @item.save
+    if @item.save
     redirect_to root_path
+    else
+    render :new
+    end
   end
+
 
   private
 
