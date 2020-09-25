e6e8374d987c147c43333df59b82570f5762ee11
kohki-kimura <kohki.soccer0217@gmail.com>
2020-09-25 16:16:06 +0900

    rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
 
   with_options presence: true do
-  validates :nickname
-  validates :birthday 
-  validates :first_name, :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/i }
-  validates :first_name_kana, :family_name_kana, format: { with: /\A[ァ-ン]+\z/i }
+    validates :nickname
+    validates :birthday
+    validates :first_name, :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/i }
+    validates :first_name_kana, :family_name_kana, format: { with: /\A[ァ-ン]+\z/i }
   end
 
   has_many :items
