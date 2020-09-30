4c1cbf4e8e1908207a72130506d17d3254de33f4
kohki-kimura <kohki.soccer0217@gmail.com>
2020-09-30 12:49:50 +0900

    rubocopの実行

--- app/models/order_donation.rb
+++ app/models/order_donation.rb
@@ -7,7 +7,7 @@ class OrderDonation
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
     validates :state_id, numericality: { other_than: 1 }
-  end  
+  end
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
